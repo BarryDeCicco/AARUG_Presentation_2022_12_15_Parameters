27e58ccd641cd7095034b8536263777eda396871
Barry DeCicco <bdecicco2001@yahoo.com>
2023-02-21 16:32:53 -0500

    All sorts of changes :(

--- inst/Create_Data_Set.R
+++ inst/Create_Data_Set.R
@@ -14,23 +14,30 @@ library(lubridate)
 set.seed(123456)
 
 
-#### Create Columns, then bind to become a data frame           ####
+#### Import Data           ####
 
-date_var <- seq(ymd('2020-01-01'),ymd('2022-12-31'),by='days')
-quarter_var <- lubridate::quarter(date_var)
-random_var <- rnorm(1096,0,1)
-adjustment <-
-data_set <- rbind(date_var, quarter_var, random_var) %>% as.data.frame()
-
-data_set <- data_set %>% ungroup() %>%
-  dplyr::mutate(adjustment = 1) %>%
-  dplyr::mutate(adjustment = as.numeric(adjustment)) %>%
-  dplyr::mutate(adjustment = ifelse(quarter_var == 1, 3,adjustment))
-
-
+data_file <- read.csv("inst/file.csv") %>%
+  dplyr::mutate(Date = as.Date(Date))
 
 #### END OF Create Columns, then bind to become a data frame           ####
 
+####  Make Graph                                                       ####
+
+region_selected <- "Europe"
+plot_title <- paste0("Line Chart for Region = ", region_selected)
+
+data_file %>%
+  dplyr::filter(Region == region_selected) %>%
+  ggplot(.,aes(x = Date, y=Value)) +
+    geom_line() +
+    labs(title=plot_title,x="Date", y = "Value")+
+    theme_classic() +
+  scale_x_date(date_breaks = "3 months",date_labels = "%m-%Y") +
+  theme(axis.text.x=element_text(angle=60, hjust=1))
+
+
+
+  region_selected <-  `r params$region`
 
 
 
